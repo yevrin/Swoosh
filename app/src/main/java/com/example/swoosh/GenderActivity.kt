eff5387be12c769f83bdf583a05dc3adaa62cd64
yevrin <klkite@uwaterloo.ca>
2019-03-10 19:08:24 -0400

    lesson 04

--- app/src/main/java/com/example/swoosh/GenderActivity.kt
+++ app/src/main/java/com/example/swoosh/GenderActivity.kt
@@ -1,12 +1,21 @@
 package com.example.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_gender.*
 
-class GenderActivity : AppCompatActivity() {
+class GenderActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gender)
+
+        fun genderNextClick(view: View){
+            val skillLevelIntent = Intent(this, SkillLevelActivity::class.java)
+            startActivity(skillLevelIntent)
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/swoosh/SkillLevelActivity.kt
@@ -0,0 +1,12 @@
+package com.example.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillLevelActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill_level)
+    }
+}
